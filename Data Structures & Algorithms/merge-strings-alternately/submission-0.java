a4e6f805a0e9d0292af381107782e896e6ff8462
Nelvin Poly <85396924+nelvinpoly@users.noreply.github.com>
2026-04-01 19:11:07 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+      // int i=0,j=0;
+      // StringBuilder res=new StringBuilder();
+      // while(i<word1.length() &&j<word2.length()){
+      //       res.append(word1.charAt(i++));
+      //       res.append(word2.charAt(j++));
+      // }  
+      // res.append(word1.substring(i));
+      // res.append(word2.substring(j));
+      // return res.toString();
+      int n=word1.length(),m=word2.length();
+      StringBuilder res=new StringBuilder();
+      int i=0,j=0;
+      while(i<n||j<m){
+        if(i<n){
+          res.append(word1.charAt(i++));
+        }
+        if(j<m){
+          res.append(word2.charAt(j++));
+        }
+      }
+      return res.toString();
+    }
+}
