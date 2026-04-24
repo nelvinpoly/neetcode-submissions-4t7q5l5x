a3817bcb664171ceaf3f4c85d710c770fff1b8b0
Nelvin Poly <85396924+nelvinpoly@users.noreply.github.com>
2026-04-24 09:02:31 +0530

    Add: length-of-last-word - submission-1

---
+++ Data Structures & Algorithms/length-of-last-word/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int i=s.length()-1;
+      int length=0;
+      while(s.charAt(i)==' '){
+        i--;
+      } 
+      while(i>=0 && s.charAt(i)!=' ') {
+        i--;
+        length++;
+      }return length;
+    }
+}
