5680410b8f4326932b85fd67fc8a157c7d397534
Nelvin Poly <85396924+nelvinpoly@users.noreply.github.com>
2026-06-10 17:51:18 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==0) return nums[1];
+        return Math.max(dfs(nums,0,n-2),dfs(nums,1,n-1));
+    }
+    public int dfs(int[] nums,int start,int end){
+        int rob1=0,rob2=0;
+        for(int i=start;i<=end;i++){
+            int top=Math.max(rob1+nums[i],rob2);
+            rob1=rob2;
+            rob2=top;
+        }return rob2;
+    }
+}
