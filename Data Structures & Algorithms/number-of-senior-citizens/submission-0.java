f2bc3ace5df77633c636fdb000b48123d6bd56e4
Nelvin Poly <85396924+nelvinpoly@users.noreply.github.com>
2026-06-11 23:12:17 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count=0;
+        for(String i:details){
+            if(Integer.parseInt(i.substring(11,13))>60){
+                count++;
+            }
+        }return count;
+    }
+}
